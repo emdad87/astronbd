73abdebdf0a92cb6996fcbe81d2c9b851f77379f
emdad87 <emdad87@hotmail.com>
2015-07-30 05:50:06 +1000

    Product Page edits

--- js/products.js
+++ js/products.js
@@ -20,13 +20,86 @@ $(document).ready(function(){
 
 	});
 	
-	$('#pdisp2').hover(function()
+	
+	
+	$('#pd_acode').hover(function()
 	{
 	  // do my image switching logic here.
-	  $('#product-display').css("background-image", "url(./images/stayon_330x330.jpg)");
+	  $('#product-display').css("background-image", "url(./images/products/acode_330x411.jpg)");
 	  $('#product-details').text("product2");
 
 	});
+	
+	$('#pd_blueray').hover(function()
+	{
+	  // do my image switching logic here.
+	  $('#product-display').css("background-image", "url(./images/products/blueray_330x434.jpg)");
+	  $('#product-details').text("product3");
+
+	});
+	
+	
+	$('#pd_bounce3').hover(function()
+	{
+	  // do my image switching logic here.
+	  $('#product-display').css("background-image", "url(./images/products/bounce3_330x378.jpg)");
+	  $('#product-details').text("product3");
+
+	});
+	
+	$('#pd_lazer').hover(function()
+	{
+	  // do my image switching logic here.
+	  $('#product-display').css("background-image", "url(./images/products/lazer_330x374.jpg)");
+	  $('#product-details').text("product3");
+
+	});
+
+	
+	$('#pd_runcool').hover(function()
+	{
+	  // do my image switching logic here.
+	  $('#product-display').css("background-image", "url(./images/products/runcool_330x278.jpg)");
+	  $('#product-details').text("product3");
+
+	});
+	
+	
+	$('#pd_speedyboss').hover(function()
+	{
+	  // do my image switching logic here.
+	  $('#product-display').css("background-image", "url(./images/products/speedyboss_330x468.jpg)");
+	  $('#product-details').text("product3");
+
+	});
+	
+	$('#pd_whitecode').hover(function()
+	{
+	  // do my image switching logic here.
+	  $('#product-display').css("background-image", "url(./images/products/whitecode_330x377.jpg)");
+	  $('#product-details').text("product3");
+
+	});
+	
+	$('#pd_youthcode').hover(function()
+	{
+	  // do my image switching logic here.
+	  $('#product-display').css("background-image", "url(./images/products/youthcode_330x475.jpg)");
+	  $('#product-details').text("product3");
+
+	});
+
+	
+	
+	/*
+	$('#pdisp3').hover(function()
+	{
+	  // do my image switching logic here.
+	  $('#product-display').css("background-image", "url(./images/facewash_330x330.jpg)");
+	  $('#product-details').text("product3");
+
+	});
+	
 	
 	$('#pdisp3').hover(function()
 	{
@@ -35,7 +108,9 @@ $(document).ready(function(){
 	  $('#product-details').text("product3");
 
 	});
-
+	*/
+	
+	
 	
 	
 	
